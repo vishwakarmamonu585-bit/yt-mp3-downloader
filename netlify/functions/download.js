54644b5a804e39e5b474f6f6d54e956a7cf491bb
vishwakarmamonu585-bit <vishwakarmamonu585@gmail.com>
2026-06-26 16:44:45 +0530

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -1,1 +1,17 @@
 
+exports.handler = async (event) => {
+    const videoId = event.queryStringParameters.videoId;
+    if (!videoId) return { statusCode: 400, body: 'Video ID नहीं मिला' };
+
+    // YouTube से डायरेक्ट ऑडियो लिंक
+    const audioUrl = `https://www.youtube.com/watch?v=${videoId}`;
+    
+    return {
+        statusCode: 200,
+        headers: {
+            'Location': audioUrl,
+            'Content-Type': 'audio/mpeg'
+        },
+        body: 'MP3 डाउनलोड शुरू हो रहा है...'
+    };
+};
