8edbb481b64ac4898eb6c272c8cd73157a6ab8f0
vishwakarmamonu585-bit <vishwakarmamonu585@gmail.com>
2026-06-26 15:35:19 +0530

    Update get-videos.js

--- netlify/functions/get-videos.js
+++ netlify/functions/get-videos.js
@@ -1,1 +1,11 @@
+exports.handler = async (event) => {
+    const url = event.queryStringParameters.url;
+    if (!url) return { statusCode: 400, body: 'URL नहीं मिला' };
 
+    const dummyData = [
+        { id: 'video1', title: 'टेस्ट वीडियो 1', thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/hqdefault.jpg' },
+        { id: 'video2', title: 'टेस्ट वीडियो 2', thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/hqdefault.jpg' },
+        { id: 'video3', title: 'टेस्ट वीडियो 3', thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/hqdefault.jpg' }
+    ];
+    return { statusCode: 200, body: JSON.stringify(dummyData) };
+};
